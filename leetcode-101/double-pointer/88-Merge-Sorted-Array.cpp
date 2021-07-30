f2cbe401feb2c9a21181815dcf74ad0df019c9cf
dejavudwh <494319000@qq.com>
2021-07-30 21:27:28 +0800

    add 88

---
+++ leetcode-101/double-pointer/88-Merge-Sorted-Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    // 类似双指针
+    // 类似归并
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int pos = m + n - 1;
+        m--;
+        n--;
+        while (m >= 0 && n >= 0) {
+            nums1[pos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+
+        while (n >= 0) {
+            nums1[pos--] = nums2[n--];
+        }
+    }
+};
